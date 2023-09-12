0eeba9cccc27aaac6868b1f256c46402a98fcdbb
Rajendra sai <rajendrasai9@gmail.com>
2023-09-13 02:05:13 +0530

    Create inr_usd.java

---
+++ first_java_prog/inr_usd.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class inr_usd{
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+		float inr = scan.nextFloat();
+		System.out.print(inr*0.012);
+
+	}
+}
