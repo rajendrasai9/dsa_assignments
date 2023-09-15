3dfffebdb67f2e1fdaccb5e9e6fc85a755b654bd
Rajendra sai <rajendrasai9@gmail.com>
2023-09-15 14:54:46 +0530

    Create Perimeter_Square.java

---
+++ Conditionals_loops/Perimeter_Square.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Perimeter_Square{
+	public static void main(String[] args){
+		Scanner scan= new Scanner(System.in);
+		System.out.println("Enter the side of square: ");
+		float side = scan.nextFloat();
+		System.out.println(4*side);
+	}
+}
