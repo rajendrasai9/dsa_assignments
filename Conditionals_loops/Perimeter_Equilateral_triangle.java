1171e7616a367f54ec1963c3ddf9ab7f31a2826b
Rajendra sai <rajendrasai9@gmail.com>
2023-09-15 14:53:58 +0530

    Create Perimeter_Equilateral_triangle.java

---
+++ Conditionals_loops/Perimeter_Equilateral_triangle.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Perimeter_Equilateral_triangle{
+	public static void main(String[] args){
+		Scanner scan= new Scanner(System.in);
+		System.out.println("Enter the side of Equilateral triangle: ");
+		float side = scan.nextFloat();
+		System.out.println(3*side);
+	}
+}
